840681cda992e8465c78b138fa7756b6547869fd
Edward <edwardvewen@163.com>
2018-12-03 09:45:37 +0000

    shouwu, mahu

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -4,6 +4,8 @@ namespace App\Observers;
 
 use App\Models\Reply;
 
+use App\Notifications\TopicReplied;
+
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
@@ -16,6 +18,13 @@ class ReplyObserver
 
         // 过滤XSS, 使用HTMLPurifier
         $reply->content = clean($reply->content, 'user_topic_body');
+    }
+
+    public function created(Reply $reply) {
+        $topic = $reply->topic;
+        $topic->increment('reply_count', 1);
+
+        $topic->user->notify(new TopicReplied($reply));
     }
 
     public function updating(Reply $reply)
