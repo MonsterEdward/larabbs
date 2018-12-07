a2276647e60b5ad3dd731a7a2d307e5cee63aa28
Edward <edwardvewen@163.com>
2018-12-07 10:04:11 +0000

    administrator

--- app/Policies/Policy.php
+++ app/Policies/Policy.php
@@ -15,6 +15,7 @@ class Policy
 
     public function before($user, $ability)
 	{
+        // https://laravel-china.org/docs/laravel/5.5/authorization/1310#policy-filters
         // 如果用户拥有管理内容的权限, 即授权通过
 	    if ($user->can('manage_contents')) { // 又拼错单词!
 	    		return true;
