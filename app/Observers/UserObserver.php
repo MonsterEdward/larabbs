88042c1a2ca7be82294843c7e95b280e3d203783
Edward <edwardvewen@163.com>
2018-12-10 08:17:37 +0000

    over

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -9,6 +9,7 @@ use App\Models\User;
 
 class UserObserver
 {
+    /*
     public function creating(User $user)
     {
         //
@@ -17,5 +18,14 @@ class UserObserver
     public function updating(User $user)
     {
         //
+    }
+    */
+
+    // 使用模型监控器来实现此功能, 在用户数据即将入库之前, 将为avatar字段设置一张默认的头像 
+    public function saving(User $user) {
+        // 这样写扩展性更高，只有空的时候才指定默认头像
+        if (empty($user->avatar)) {
+            $user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png';
+        }
     }
 }
